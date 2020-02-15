9b81836db647bfb37c8c4f758860c102da0dc6bf
Felipe Yuri Silva <felipe.yuri.silva@everis.com>
2020-02-15 04:18:30 -0300

    Criado recyclerview

--- app/src/main/java/com/example/cevadabeer/activities/ProducaoActivity.java
+++ app/src/main/java/com/example/cevadabeer/activities/ProducaoActivity.java
@@ -5,11 +5,15 @@ import android.os.Bundle;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.cevadabeer.R;
+import com.example.cevadabeer.control.MyRecyclerViewAdapter;
 import com.example.cevadabeer.entities.Producao;
 import com.example.cevadabeer.services.RetrofitConfig;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
@@ -18,13 +22,17 @@ import retrofit2.Response;
 
 public class ProducaoActivity extends AppCompatActivity {
 
+    private ArrayList<Producao> lista = new ArrayList<>();
 
+    //Configurando o gerenciador de layout para ser uma lista
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_production);
         carregarListaProducao();
+
+
     }
 
     public void carregarListaProducao() {
@@ -40,7 +48,7 @@ public class ProducaoActivity extends AppCompatActivity {
                 if (!response.isSuccessful()) {
                     Log.e("ProducaoService", "Código: " + response.code());
                 }
-                this.lista = response.body();
+                lista = response.body();
             }
 
             @Override
@@ -48,7 +56,8 @@ public class ProducaoActivity extends AppCompatActivity {
                 Log.e("IngredienteService   ", "Erro ao buscar ingrediente" + t.getMessage());
             }
         });
-
     }
+
+
 }
 
